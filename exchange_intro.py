c8c66584c1a2f3e611eacebe360616f931fd8df1
Axel-Jacobsen <axelnj44@gmail.com>
2018-10-17 13:04:24 -0700

    Start to explore

--- exchange_intro.py
+++ exchange_intro.py
@@ -1,15 +1,9 @@
 import ccxt
+import json
 
-print(ccxt.exchanges)
+quadrigacx   = ccxt.quadrigacx()
 
-hitbtc = ccxt.hitbtc({'verbose': True})
-bitmex = ccxt.bitmex()
-huobi  = ccxt.huobi()
-hitbtc_markets = hitbtc.load_markets()
-
-print(hitbtc.id, hitbtc_markets)
-print(bitmex.id, bitmex.load_markets())
-print(huobi.id, huobi.load_markets())
-
-print(hitbtc.fetch_order_book(hitbtc.symbols[0]))
-print(bitmex.fetch_ticker('BTC/USD'))
+with open('quote.json', 'w') as f:
+    json.dump(quadrigacx.fetch_ticker('BTC/CAD'), f)
+    
+  
