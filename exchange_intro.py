3c99096b5c9fa4df6c8624ec8d15ff6b0e25bc45
Axel-Jacobsen <axelnj44@gmail.com>
2018-10-23 11:17:34 -0700

    explore volturo

--- exchange_intro.py
+++ exchange_intro.py
@@ -1,9 +1,20 @@
 import ccxt
 import json
 
-quadrigacx   = ccxt.quadrigacx()
+quadrigacx = ccxt.quadrigacx()
+vaultoro   = ccxt.vaultoro()
 
 with open('quote.json', 'w') as f:
-    json.dump(quadrigacx.fetch_ticker('BTC/CAD'), f)
+    # json.dump(quadrigacx.fetch_ticker('BTC/CAD'), f)
+    json.dump(vaultoro.fetch_ticker('BTC/USD'), f)
     
-  
+
+def get_spread(exchange, ticker):
+    orderbook = exchange.fetch_order_book(ticker)
+    bid = orderbook['bids'][0][0] if len (orderbook['bids']) > 0 else None
+    ask = orderbook['asks'][0][0] if len (orderbook['asks']) > 0 else None
+    spread = (ask - bid) if (bid and ask) else None
+    return (exchange.id, 'market price', { 'bid':  bid, 'ask': ask, 'spread': spread })
+
+print(get_spread(quadrigacx, 'BTC/CAD'))
+print(get_spread(vaultoro, 'BTC/USD'))
