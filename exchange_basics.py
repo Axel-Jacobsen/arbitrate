e6fe2cbbbf07fec17dfa7e9e824615e011e50276
Axel-Jacobsen <axelnj44@gmail.com>
2018-10-23 11:51:19 -0700

    rename exchange_intro

--- exchange_basics.py
+++ exchange_basics.py
@@ -1,9 +1,6 @@
 import ccxt
 import json
 
-quadrigacx = ccxt.quadrigacx()
-binance    = ccxt.binance()
-
 def save_json(json_data, filename):
     with open(filename, 'w') as f:
         json.dump(json_data, f)
@@ -23,6 +20,9 @@ def analyze_raw_tickers(json_data):
         print(ticker, json_data[ticker]['bid'])
 
 if __name__ == '__main__':
+    quadrigacx = ccxt.quadrigacx()
+    binance    = ccxt.binance()
+    
     save_json(quadrigacx.fetch_ticker('BTC/CAD'), 'miscellaneous_data/btccad.json')
     save_json(get_tickers(quadrigacx), 'ticker_data/quadrigacx_tickers.json')
     save_json(get_tickers(binance), 'ticker_data/binance_tickers.json')
