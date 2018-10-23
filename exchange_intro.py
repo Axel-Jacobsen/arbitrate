b38d5c5d67dd161f592728db268814961e815cb9
Axel-Jacobsen <axelnj44@gmail.com>
2018-10-23 11:27:10 -0700

    update exchange

--- exchange_intro.py
+++ exchange_intro.py
@@ -2,12 +2,10 @@ import ccxt
 import json
 
 quadrigacx = ccxt.quadrigacx()
-vaultoro   = ccxt.vaultoro()
 
-with open('quote.json', 'w') as f:
-    # json.dump(quadrigacx.fetch_ticker('BTC/CAD'), f)
-    json.dump(vaultoro.fetch_ticker('BTC/USD'), f)
-    
+def save_json(json_data, filename):
+    with open(filename, 'w') as f:
+        json.dump(json_data, f)
 
 def get_spread(exchange, ticker):
     orderbook = exchange.fetch_order_book(ticker)
@@ -16,5 +14,14 @@ def get_spread(exchange, ticker):
     spread = (ask - bid) if (bid and ask) else None
     return (exchange.id, 'market price', { 'bid':  bid, 'ask': ask, 'spread': spread })
 
-print(get_spread(quadrigacx, 'BTC/CAD'))
-print(get_spread(vaultoro, 'BTC/USD'))
+def get_tickers(exchange):
+    return exchange.fetch_tickers()
+
+def analyze_raw_tickers(json_data):
+    for ticker in json_data:
+        print(ticker, json_data[ticker]['bid'])
+
+if __name__ == '__main__':
+    save_json(quadrigacx.fetch_ticker('BTC/CAD'), 'btccad.json')
+    save_json(get_tickers(quadrigacx), 'quadrigacx_tickers.json')
+    analyze_raw_tickers(get_tickers(quadrigacx))
